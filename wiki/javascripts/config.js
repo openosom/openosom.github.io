f3c6c063d75d55dea3fbee72b734703807ecae47
Neil <openosom@gmail.com>
2014-06-09 19:57:15 +0800

    wiki更换页面

---
+++ wiki/javascripts/config.js
@@ -0,0 +1,11 @@
+var disqus_shortname = 'openosom';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://www.openosom.org/wiki';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'openosom';						//Your username of GitHub.
+var repos = 'openosom.github.io';					//Name of repository that contains your blog.
+var subdir ='wiki'; 							// None'', Wiki'wiki', Blog'blog'
+var mddir ='../md';							//files of Markdown
+var sitetitle = 'OpenOSOM wiki';						//Name of your blog.
+var rss = '';									//Rss address of your blog.
+var suffix = '.md';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+
