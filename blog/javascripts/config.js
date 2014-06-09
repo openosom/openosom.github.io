13fe38f8fcec1839eca5edf60425f988eaa67d7e
Neil <openosom@gmail.com>
2014-06-09 10:38:11 +0800

    修复blog

--- blog/javascripts/config.js
+++ blog/javascripts/config.js
@@ -1,7 +1,7 @@
 var disqus_shortname = 'OpenOSOM';			//Short Name of disqus, this parameter is for disqus comment box.
 var hostbase = 'http://www.openosom.org/blog';		//The absolute root address of your blog without '/' in the end.
 var githubname = 'openosom';						//Your username of GitHub.
-var repos = 'openosom.github.io';					//Name of repository that contains your blog.
+var repos = 'blog';					//Name of repository that contains your blog.
 var sitetitle = 'OpenOSOM Blog';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
